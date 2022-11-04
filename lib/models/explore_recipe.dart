9c2c8623dba7a166f54191b3a9a35d271d22113e
le.quang.dao <le.quang.dao@sun-asterisk.com>
2022-11-04 09:08:10 +0700

    init models

---
+++ lib/models/explore_recipe.dart
@@ -0,0 +1,119 @@
+class ExploreRecipe {
+  String id;
+  String cardType;
+  String title;
+  String subtitle;
+  String backgroundImage;
+  String backgroundImageSource;
+  String message;
+  String authorName;
+  String role;
+  String profileImage;
+  int durationInMinutes;
+  String dietType;
+  int calories;
+  List<String> tags;
+  String description;
+  String source;
+  List<Ingredients> ingredients;
+  List<Instruction> instructions;
+
+  ExploreRecipe({
+    required this.id,
+    required this.cardType,
+    required this.title,
+    this.subtitle = '',
+    this.backgroundImage = '',
+    this.backgroundImageSource = '',
+    this.message = '',
+    this.authorName = '',
+    this.role = '',
+    this.profileImage = '',
+    this.durationInMinutes = 0,
+    this.dietType = '',
+    this.calories = 0,
+    this.tags = const [],
+    this.description = '',
+    this.source = '',
+    this.ingredients = const [],
+    this.instructions = const [],
+  });
+
+  factory ExploreRecipe.fromJson(Map<String, dynamic> json) {
+    final ingredients = <Ingredients>[];
+    final instructions = <Instruction>[];
+
+    if (json['ingredients'] != null) {
+      json['ingredients'].forEach((jsonElement) {
+        ingredients.add(Ingredients.fromJson(jsonElement));
+      });
+    }
+
+    if (json['instructions'] != null) {
+      json['instructions'].forEach((jsonElement) {
+        instructions.add(Instruction.fromJson(jsonElement));
+      });
+    }
+
+    return ExploreRecipe(
+      id: json['id'] ?? '',
+      cardType: json['cardType'] ?? '',
+      title: json['title'] ?? '',
+      subtitle: json['subtitle'] ?? '',
+      backgroundImage: json['backgroundImage'] ?? '',
+      backgroundImageSource: json['backgroundImageSource'] ?? '',
+      message: json['message'] ?? '',
+      authorName: json['authorName'] ?? '',
+      role: json['role'] ?? '',
+      profileImage: json['profileImage'] ?? '',
+      durationInMinutes: json['durationInMinutes'] ?? 0,
+      dietType: json['dietType'] ?? '',
+      calories: json['calories'] ?? 0,
+      tags: json['tags'].cast<String>() ?? [],
+      description: json['description'] ?? '',
+      source: json['source'] ?? '',
+      ingredients: ingredients,
+      instructions: instructions,
+    );
+  }
+}
+
+class Ingredients {
+  String imageUrl;
+  String title;
+  String source;
+
+  Ingredients({
+    required this.imageUrl,
+    required this.title,
+    required this.source,
+  });
+
+  factory Ingredients.fromJson(Map<String, dynamic> json) {
+    return Ingredients(
+      imageUrl: json['imageUrl'] ?? '',
+      title: json['title'] ?? '',
+      source: json['source'] ?? '',
+    );
+  }
+}
+
+class Instruction {
+  String imageUrl;
+  String description;
+  int durationInMinutes;
+
+  Instruction({
+    required this.imageUrl,
+    required this.description,
+    required this.durationInMinutes,
+  });
+
+  factory Instruction.fromJson(Map<String, dynamic> json) {
+    return Instruction(
+      imageUrl: json['imageUrl'] ?? '',
+      description: json['description'] ?? '',
+      durationInMinutes: json['durationInMinutes'] ?? '',
+    );
+  }
+}
