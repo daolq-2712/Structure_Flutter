4ceb062d35ca8c856c4a20eb4bdbb31bee1bb428
le.quang.dao <le.quang.dao@sun-asterisk.com>
2022-11-07 12:22:37 +0700

    Build grocery screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:structureflutter/models/grocery_manager.dart';
 import 'home.dart';
 import 'theme/fooderlich_theme.dart';
 import 'package:provider/provider.dart';
@@ -22,7 +23,10 @@ class Fooderlich extends StatelessWidget {
       title: 'Fooderlich',
       // 4
       home: MultiProvider(
-        providers: [ChangeNotifierProvider(create: (context) => TabManager())],
+        providers: [
+          ChangeNotifierProvider(create: (context) => TabManager()),
+          ChangeNotifierProvider(create: (context) => GroceryManager())
+        ],
         child: const Home(),
       ),
     );
--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:structureflutter/models/grocery_manager.dart';
 import 'empty_grocery_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
@@ -7,7 +9,27 @@ class GroceryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 // TODO 4: Add a scaffold widget
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // TODO 11: Present GroceryItemScreen
+        },
+      ),
+      body: _buildGroceryScreen(),
+    );
   }
-// TODO: Add buildGroceryScreen
+
+  Widget _buildGroceryScreen() {
+    return Consumer<GroceryManager>(
+      builder: (context, manager, child) {
+        if (manager.groceryItems.isNotEmpty) {
+          // TODO 25: Add grocery screen
+          return Container();
+        } else {
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
+  }
 }
