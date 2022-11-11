1f174a772e790ec14c179784d4107c71fdf09610
le.quang.dao <le.quang.dao@sun-asterisk.com>
2022-11-11 17:28:05 +0700

    Implement Chap 12

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -1,35 +1,9 @@
-import 'package:flutter/foundation.dart';
-import 'package:http/http.dart';
+import 'package:chopper/chopper.dart';
+import 'package:structureflutter/network/model_response.dart';
 
-import '../env/debug_env.dart';
+import 'recipe_model.dart';
 
-class RecipeService {
-  // 1
-  Future _getData(String url) async {
-    // 2
-    if (kDebugMode) {
-      print('Calling url: $url');
-    }
-    // 3
-    final response = await get(Uri.parse(url));
-    // 4
-    if (response.statusCode == 200) {
-      // 5
-      return response.body;
-    } else {
-      // 6
-      if (kDebugMode) {
-        print(response.statusCode);
-      }
-    }
-  }
+abstract class RecipeService extends ChopperService {
 
-  // 1
-  Future<dynamic> getRecipes(String query, int from, int to) async {
-// 2
-    final recipeData = await _getData(
-        '${DebugEnv.API_URL}?app_id=${DebugEnv.API_ID}&app_key=${DebugEnv.API_KEY}&q=$query&from=$from&to=$to');
-    // 3
-    return recipeData;
-  }
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(@Query('q')String query,@Query('from') int from,@Query('to') int to);
 }
