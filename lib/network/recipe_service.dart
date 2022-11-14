d02fdc2d85cc07b59279c062795608cc5d19a27c
le.quang.dao <le.quang.dao@sun-asterisk.com>
2022-11-14 10:28:39 +0700

    Chapter 12: Networking using chopper library

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -1,9 +1,33 @@
 import 'package:chopper/chopper.dart';
-import 'package:structureflutter/network/model_response.dart';
 
+import '../env/debug_env.dart';
+import '../network/model_response.dart';
+import '../network/model_converter.dart';
 import 'recipe_model.dart';
 
-abstract class RecipeService extends ChopperService {
+part 'recipe_service.chopper.dart';
 
-  Future<Response<Result<APIRecipeQuery>>> queryRecipes(@Query('q')String query,@Query('from') int from,@Query('to') int to);
+@ChopperApi(baseUrl: DebugEnv.API_URL)
+abstract class RecipeService extends ChopperService {
+  @Get(path: '/search')
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+      @Query('q') String query, @Query('from') int from, @Query('to') int to);
+
+  static RecipeService create() {
+    final client = ChopperClient(
+      baseUrl: DebugEnv.API_URL,
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      converter: ModelConverter(),
+      errorConverter: const JsonConverter(),
+      services: [_$RecipeService()],
+    );
+    return _$RecipeService(client);
+  }
+}
+
+Request _addQuery(Request req) {
+  final params = Map<String, dynamic>.from(req.parameters);
+  params['app_id'] = DebugEnv.API_ID;
+  params['app_key'] = DebugEnv.API_KEY;
+  return req.copyWith(parameters: params);
 }
